2331bd01a08d29bf613bafd228a96fae658d59e1
Kharoh <Gaulu03@gmail.com>
2023-11-26 22:19:07 +0100

    added gallery and footer

--- src/main.js
+++ src/main.js
@@ -13,7 +13,6 @@ let color1 = 'white'
 let color2 = 'blanchedalmond'
 let animation = true
 
-
 const updateCanvas = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
@@ -48,3 +47,55 @@ const updateCanvas = () => {
 }
 
 setTimeout(updateCanvas, 1000 / 60 / vitesse)
+
+const cardioidCanvas = document.getElementById('cardioid')
+cardioidCanvas.width = 1000
+cardioidCanvas.height = 1000
+const cardioidCtx = cardioidCanvas.getContext('2d')
+
+for (let i = 0; i < 1000; i++) {
+    const line = [
+        { x: Math.cos(2 * Math.PI / 1000 * i), y: Math.sin(2 * Math.PI / 1000 * i) },
+        { x: Math.cos(2 * Math.PI / 1000 * i * 2), y: Math.sin(2 * Math.PI / 1000 * i * 2) }
+    ]
+    drawExtendedLine(cardioidCtx, 500 + line[0].x * 400, 500 + line[0].y * 400, 500 + line[1].x * 400, 500 + line[1].y * 400, prolongement, largeur, color1, color2)
+}
+
+const trefleCanvas = document.getElementById('trefle')
+trefleCanvas.width = 1000
+trefleCanvas.height = 1000
+const trefleCtx = trefleCanvas.getContext('2d')
+
+for (let i = 0; i < lignes; i++) {
+    const line = [
+        { x: Math.cos(2 * Math.PI / lignes * i), y: Math.sin(2 * Math.PI / lignes * i) },
+        { x: Math.cos(2 * Math.PI / lignes * i * 4), y: Math.sin(2 * Math.PI / lignes * i * 4) }
+    ]
+    drawExtendedLine(trefleCtx, 500 + line[0].x * 400, 500 + line[0].y * 400, 500 + line[1].x * 400, 500 + line[1].y * 400, prolongement, largeur, color1, color2)
+}
+
+const nothingCanvas = document.getElementById('nothing')
+nothingCanvas.width = 1000
+nothingCanvas.height = 1000
+const nothingCtx = nothingCanvas.getContext('2d')
+
+for (let i = 0; i < lignes; i++) {
+    const line = [
+        { x: Math.cos(2 * Math.PI / lignes * i), y: Math.sin(2 * Math.PI / lignes * i) },
+        { x: Math.cos(2 * Math.PI / lignes * i * 250), y: Math.sin(2 * Math.PI / lignes * i * 250) }
+    ]
+    drawExtendedLine(nothingCtx, 500 + line[0].x * 400, 500 + line[0].y * 400, 500 + line[1].x * 400, 500 + line[1].y * 400, prolongement, largeur, color1, color2)
+}
+
+const lotusCanvas = document.getElementById('lotus')
+lotusCanvas.width = 1000
+lotusCanvas.height = 1000
+const lotusCtx = lotusCanvas.getContext('2d')
+
+for (let i = 0; i < lignes; i++) {
+    const line = [
+        { x: Math.cos(2 * Math.PI / lignes * i), y: Math.sin(2 * Math.PI / lignes * i) },
+        { x: Math.cos(2 * Math.PI / lignes * i * 467), y: Math.sin(2 * Math.PI / lignes * i * 467) }
+    ]
+    drawExtendedLine(lotusCtx, 500 + line[0].x * 400, 500 + line[0].y * 400, 500 + line[1].x * 400, 500 + line[1].y * 400, prolongement, largeur, color1, color2)
+}
